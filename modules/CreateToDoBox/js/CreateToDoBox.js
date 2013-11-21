371d54a84f98a254af8e4f5841db019bdd18ff65
Samir Mumic <samir.mumic@namics.com>
2013-11-21 13:05:19 +0100

    Add Modules CreateToDoBox and ToDoBox

---
+++ modules/CreateToDoBox/js/CreateToDoBox.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: smumic
+ * Date: 21.11.13
+ * Time: 12:39
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ modules/ToDoBox/js/ToDoBox.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: smumic
+ * Date: 21.11.13
+ * Time: 11:08
+ * To change this template use File | Settings | File Templates.
+ */
