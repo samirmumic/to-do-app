04decd7a2325bb601e8689f92f42eadafde2ec0b
Samir Mumic <samir.mumic@namics.com>
2013-11-22 15:16:16 +0100

    Add Javascript functions

--- modules/CreateToDoBox/js/CreateToDoBox.js
+++ modules/CreateToDoBox/js/CreateToDoBox.js
@@ -1,7 +1,66 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: smumic
- * Date: 21.11.13
- * Time: 12:39
- * To change this template use File | Settings | File Templates.
- */
+(function ($) {
+	/**
+	 * Default module implementation.
+	 *
+	 * @author Remo Brunschwiler
+	 * @namespace Tc.Module
+	 * @class Default
+	 * @extends Tc.Module
+	 */
+	Tc.Module.CreateToDoBox = Tc.Module.extend({
+
+		/**
+		 * Initializes the Default module.
+		 *
+		 * @method init
+		 * @constructor
+		 * @param {jQuery|Zepto} $ctx the jquery context
+		 * @param {Sandbox} sandbox the sandbox to get the resources from
+		 * @param {String} modId the unique module id
+		 */
+		init: function ($ctx, sandbox, modId) {
+			// call base constructor
+			this._super($ctx, sandbox, modId);
+		},
+
+		/**
+		 * Hook function to do all of your module stuff.
+		 *
+		 * @method on
+		 * @param {Function} callback function
+		 * @return void
+		 */
+		on: function (callback) {
+
+			// Per jQuery Button Selektieren
+			// On Click Event abfangen
+			// console.log(text aus dem inputfeld)
+
+			var $button = $(".inputcreate"),
+				$inputval = $('.inputtxt'),
+				$todobox = $('.mod-to-do-item-template'),
+				$inputcheck = $('.inputcheck');
+
+
+			$button.on('click', function (e) {
+				e.preventDefault();
+				// Die Variable valofinput wird mit dem Value vom Inputfeld abgefüllt
+				var $valofinput = $inputval.val();
+				console.log($valofinput);
+				$todobox.clone().prependTo(".mod-to-do-box").addClass("show").append($valofinput);
+			});
+
+
+			callback();
+		},
+
+		/**
+		 * Hook function to trigger your events.
+		 *
+		 * @method after
+		 * @return void
+		 */
+		after: function () {
+		}
+	});
+})(Tc.$);
